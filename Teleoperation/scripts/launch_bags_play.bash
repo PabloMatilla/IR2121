7173bab7bb4e9d4df9dee33e90ff76223b323fec
PabloMatilla <al415550@uji.es>
2023-09-13 16:29:41 +0200

    Teleoperation task in simulation

---
+++ Teleoperation/scripts/launch_bags_play.bash
@@ -0,0 +1,3 @@
+source /opt/ros/foxy/setup.bash
+export ROS_LOCALHOST_ONLY=1
+ros2 bag play rosbag2_2023_09_13-15_36_26/rosbag2_2023_09_13-15_36_26_0.db3
---
+++ Teleoperation/scripts/launch_rviz.bash
@@ -0,0 +1,3 @@
+source /opt/ros/foxy/setup.bash
+export ROS_LOCALHOST_ONLY=1
+rviz2 -d config.rviz
