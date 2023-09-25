164e418ecf5ccdeef6a6499c4e0bb644f09e3360
PabloMatilla <al415550@uji.es>
2023-09-25 18:56:49 +0200

    trayectory fisico

--- Teleoperation/scripts/launch_bags_play1.bash
+++ Teleoperation/scripts/launch_bags_play1.bash
@@ -1,3 +1,4 @@
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
-ros2 bag play rosbag2_2023_09_22-16_38_11/rosbag2_2023_09_22-16_38_11_0.db3 
+ros2 bag play rosbag2_2023_09_25-16_17_50/rosbag2_2023_09_25-16_17_50_0.db3 
+
