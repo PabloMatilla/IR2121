e53ee1721db9d55834899eff287e7daaa4a4ded5
PabloMatilla <al415550@uji.es>
2023-09-27 17:10:49 +0200

    Update launch_robot-phisical_bags.bash

--- Teleoperation/scripts/launch_robot-phisical_bags.bash
+++ Teleoperation/scripts/launch_robot-phisical_bags.bash
@@ -1,7 +1,7 @@
 ssh ubuntu@169.254.XXX.YYY
-
+turtlebot
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
 
-ros2 bag record /clock /odom /tf /tf_static
+ros2 bag record /odom /tf /tf_static /scan /clock
