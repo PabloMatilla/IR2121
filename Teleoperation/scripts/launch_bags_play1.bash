8e823b1b7fdbe1a369eb8d9159c099e49d453398
PabloMatilla <al415550@uji.es>
2023-10-01 21:30:20 +0200

    rosbag upload

--- Teleoperation/scripts/launch_bags_play1.bash
+++ Teleoperation/scripts/launch_bags_play1.bash
@@ -1,4 +1,4 @@
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
-ros2 bag play rosbag2_2023_09_25-16_17_50/rosbag2_2023_09_25-16_17_50_0.db3 
+ros2 bag play rosbag2_2023_09_29-08_17_23
 
