ecf64f48591b5f8e8ad095b8dd65b3ffe23680f5
PabloMatilla <al415550@uji.es>
2023-10-11 15:27:26 +0200

    Update publish_transform.bash

--- Worlds/scripts/publish_transform.bash
+++ Worlds/scripts/publish_transform.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
-ros2 run tf2_ros static_transform_publisher -18.1 -1.0 0 0 0 0 1 map odom
+ros2 run tf2_ros static_transform_publisher 4.0 20.3 0 0 0 0 1 map odom
