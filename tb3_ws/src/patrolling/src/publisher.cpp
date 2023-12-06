cf534c305a5be5017a01715bc97c5de0b4fff6bd
PabloMatilla <al415550@uji.es>
2023-12-06 18:50:59 +0100

    CORRECT Version 3: send a sequence of goals

--- tb3_ws/src/patrolling/src/publisher.cpp
+++ tb3_ws/src/patrolling/src/publisher.cpp
@@ -12,23 +12,20 @@
 using namespace std::chrono_literals;
 
 double Vx, Vy;
-double Vx_ang, Vy_ang, Vz_ang;
 
 double x, y;
 double ang;
+
+double Vo = 4.000e-06  ;
 
 void topic_callback1(const nav_msgs::msg::Odometry::SharedPtr msg) {
     // Obtener la velocidad x y y
     Vx = msg->twist.twist.linear.x;
     Vy = msg->twist.twist.linear.y;
 
-    Vx_ang = msg->twist.twist.angular.x;
-    Vy_ang = msg->twist.twist.angular.y;
-    Vz_ang = msg->twist.twist.angular.z;
 
-}
+    /*
 
-void topic_callback2(const nav_msgs::msg::Odometry::SharedPtr msg) {
     // Obtener la posición x y y
     x = msg->pose.pose.position.x;
     y = msg->pose.pose.position.y;
@@ -48,6 +45,8 @@ void topic_callback2(const nav_msgs::msg::Odometry::SharedPtr msg) {
     std::cout << "\tx: " << x << std::endl;
     std::cout << "\ty: " << y << std::endl;
     std::cout << "\tAngulo: " << ang << std::endl;
+
+    */
 }
 
 
@@ -56,45 +55,22 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
 
-  auto subscription1 = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback1);
-  auto subscription2 = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/amcl_pose", 10, topic_callback2);
+  auto subscription1 = node->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, topic_callback1);
+  //auto subscription2 = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/amcl_pose", 10, topic_callback2);
 
-  auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
+  auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("/goal_pose", 10);
   geometry_msgs::msg::PoseStamped goal_pose_message;
+  
   rclcpp::WallRate loop_rate(500ms);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
 
   bool flag1 = 1, flag2 = 0, flag3 = 0, flag4 = 0;
 
   std::cout << "Primer goal" << std::endl;
-
   
   while (rclcpp::ok() and flag1) {
 
-    goal_pose_message.pose.position.x = 0.17; //-7.95;
-    goal_pose_message.pose.position.y = 6.44; //3.12;
-    goal_pose_message.pose.position.z = 0;
-    goal_pose_message.pose.orientation.x = 0;
-    goal_pose_message.pose.orientation.y = 0;
-    goal_pose_message.pose.orientation.z = 0;
-    goal_pose_message.pose.orientation.w = 1;
-    publisher->publish(goal_pose_message);
-    sleep(3);
-
-    std::cout << "Vx: " << Vx << std::endl;
-    std::cout << "Vy: " << Vy << std::endl;
-
-    if ((Vx == 0.01 and Vy == 0.01)){
-      std::cout << "Primer bucle" << std::endl;
-      flag1 = 0;
-      flag2 = 1;
-    }
-   
-  }
-
-   std::cout << "Segundo goal" << std::endl;
-
-  while (rclcpp::ok() and flag2) {
-  
     goal_pose_message.pose.position.x = -2.8;// -15.69;
     goal_pose_message.pose.position.y = 1.25; //32.3366;
     goal_pose_message.pose.position.z = 0;//0;
@@ -102,16 +78,41 @@ int main(int argc, char * argv[])
     goal_pose_message.pose.orientation.y = 0;
     goal_pose_message.pose.orientation.z = 0;
     goal_pose_message.pose.orientation.w = 1;
-    publisher->publish(goal_pose_message);
-    sleep(3);
 
-    if ((Vx == 0.001 and Vy == 0.001)){
-      std::cout << "Segundo bucle" << std::endl;
+   publisher->publish(goal_pose_message);
+   sleep(4);
+   rclcpp::spin_some(node);
+   loop_rate.sleep();
+ 
+
+    if (Vx < Vo and Vy < Vo and Vx != 0 ){
+      flag1 = 0;
+      flag2 = 1;
+    }
+  }
+
+   std::cout << "Segundo goal" << std::endl;
+
+  while (rclcpp::ok() and flag2) {
+  
+    goal_pose_message.pose.position.x = 5.14; // -0.06;
+    goal_pose_message.pose.position.y = 5.36; //33.52;
+    goal_pose_message.pose.position.z = 0;
+    goal_pose_message.pose.orientation.x = 0;
+    goal_pose_message.pose.orientation.y = 0;
+    goal_pose_message.pose.orientation.z = 0;
+    goal_pose_message.pose.orientation.w = 1;
+    
+    publisher->publish(goal_pose_message);
+    sleep(4);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+   
+
+    if (Vx < Vo and Vy < Vo){
       flag2 = 0;
       flag3 = 1;
     }
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
   }
 
   std::cout << "Tercer goal" << std::endl;
@@ -125,19 +126,19 @@ int main(int argc, char * argv[])
     goal_pose_message.pose.orientation.y = 0;
     goal_pose_message.pose.orientation.z = 0;
     goal_pose_message.pose.orientation.w = 1;
+    
     publisher->publish(goal_pose_message);
-    sleep(3);
+    sleep(4);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
 
-    if ((Vx == 0.001 and Vy == 0.001)){
-      std::cout << "Tercer bucle" << std::endl;
+    if (Vx < Vo and Vy < Vo){
       flag3 = 0;
       flag4 = 1;
     }
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
   }
 
-  std::cout << "Cuarto bucle" << std::endl;
+  std::cout << "Cuarto goal" << std::endl;
 
   while (rclcpp::ok() and flag4) {
 
@@ -149,12 +150,8 @@ int main(int argc, char * argv[])
     goal_pose_message.pose.orientation.z = 0;
     goal_pose_message.pose.orientation.w = 1;
     publisher->publish(goal_pose_message);
-    sleep(3);
-
-    if ((Vx == 0.001 and Vy == 0.001)){
-      std::cout << "Cuarto bucle" << std::endl;
-      flag4 = 0;
-    }
+    flag4 = 0;
+    sleep(4);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
