25fabcff8cd2cfb2e653b663de91bb7903c88702
PabloMatilla <al415550@uji.es>
2024-01-14 15:58:12 +0100

    declaración bucle for

--- tb3_ws/src/patrolling/src/publisher.cpp
+++ tb3_ws/src/patrolling/src/publisher.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <vector>
 #include "rclcpp/clock.hpp"
 #include <unistd.h>
 #include "rclcpp/rclcpp.hpp"
@@ -50,150 +51,51 @@ int main(int argc, char * argv[])
   geometry_msgs::msg::TransformStamped transformStamped;
 
 
-  bool flag1 = 1, flag2 = 0, flag3 = 0, flag4 = 0;
-
   double a, b;
+  bool flag1;
 
-  std::cout << "Primer goal" << std::endl;
-  
-  while (rclcpp::ok() and flag1) {
-                                      // laboratory    house_simulation
-    goal_pose_message.pose.position.x = a = -24.73; //  -2.8;
-    goal_pose_message.pose.position.y = b =  -8.78; //  1.25; 
-    goal_pose_message.pose.position.z = 0;
-    goal_pose_message.pose.orientation.x = 0;
-    goal_pose_message.pose.orientation.y = 0;
-    goal_pose_message.pose.orientation.z = 0;
-    goal_pose_message.pose.orientation.w = 1;
-    publisher->publish(goal_pose_message);
-    sleep(4); rclcpp::spin_some(node);
+  std::vector<double> house_sim_x = {-2.8, 5.14, 10.09, 1.62};
+  std::vector<double> house_sim_y = {1.25, 5.36, -1.91, 6.08};
 
-   try {
-      transformStamped = tfBuffer.lookupTransform("base_link", "map", rclcpp::Time(0));
-      } catch (tf2::TransformException &ex) {
-          RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
-          continue;
-       }
+  std::vector<double> laboratory_x = {-24.73, -28.50, -20.24, 24.73};
+  std::vector<double> laboratory_y = {-8.78, -20.83, -22.23, -8.78};
 
-   x = - transformStamped.transform.translation.x;
-   y = - transformStamped.transform.translation.y;
+  for (int i=0; i<4; i++){  
+      flag1 = 1;
 
-   std::cout << "a: " << a << "     x: "    << x << std::endl;
-   std::cout << "b: " << b << "     y: "    << y << std::endl;
+      while (rclcpp::ok() and flag1) {
+                                        
+        goal_pose_message.pose.position.x = a = house_sim_x[i];
+        goal_pose_message.pose.position.y = b = house_sim_y[i];
+        goal_pose_message.pose.position.z = 0;
+        goal_pose_message.pose.orientation.x = 0;
+        goal_pose_message.pose.orientation.y = 0;
+        goal_pose_message.pose.orientation.z = 0;
+        goal_pose_message.pose.orientation.w = 1;
+        publisher->publish(goal_pose_message);
+        sleep(4); rclcpp::spin_some(node);
 
-    if (((Vx < Vo) and (Vy < Vo) and (Vx != 0)) and (((a - error) < x) and (x < (a + error))) and (((b - error) < y) and (y < (b + error)))){
-      flag1 = 0;
-      flag2 = 1;
-    }
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
+      try {
+          transformStamped = tfBuffer.lookupTransform("base_link", "map", rclcpp::Time(0));
+          } catch (tf2::TransformException &ex) {
+              RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
+              continue;
+          }
+
+      x = - transformStamped.transform.translation.x;
+      y = - transformStamped.transform.translation.y;
+
+      std::cout << "a: " << a << "     x: "    << x << std::endl;
+      std::cout << "b: " << b << "     y: "    << y << std::endl;
+
+        if (((Vx < Vo) and (Vy < Vo) and (Vx != 0)) and (((a - error) < x) and (x < (a + error))) and (((b - error) < y) and (y < (b + error)))){
+          flag1 = 0;
+        }
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+      }
+
   }
-
-   std::cout << "Segundo goal" << std::endl;
-
-  while (rclcpp::ok() and flag2) {
-  
-    goal_pose_message.pose.position.x = a = -28.50;  //  5.14; 
-    goal_pose_message.pose.position.y = b = -20.83;  //  5.36; 
-    goal_pose_message.pose.position.z = 0;
-    goal_pose_message.pose.orientation.x = 0;
-    goal_pose_message.pose.orientation.y = 0;
-    goal_pose_message.pose.orientation.z = 0;
-    goal_pose_message.pose.orientation.w = 1;
-    publisher->publish(goal_pose_message);
-    sleep(4); rclcpp::spin_some(node);
-
-    try {
-      transformStamped = tfBuffer.lookupTransform("base_link", "map", rclcpp::Time(0));
-      } catch (tf2::TransformException &ex) {
-          RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
-          continue;
-       }
-
-   x = - transformStamped.transform.translation.x;
-   y = - transformStamped.transform.translation.y;
-
-   std::cout << "a: " << a << "     x: "    << x << std::endl;
-   std::cout << "b: " << b << "     y: "    << y << std::endl;
-   
-
-    if (((Vx < Vo) and (Vy < Vo) and (Vx != 0)) and (((a - error) < x) and (x < (a + error))) and (((b - error) < y) and (y < (b + error)))){
-      flag2 = 0;
-      flag3 = 1;
-    }
-  }
-
-  std::cout << "Tercer goal" << std::endl;
-
-  while (rclcpp::ok() and flag3) {
-
-    goal_pose_message.pose.position.x = a =  -20.24; //   10.09;
-    goal_pose_message.pose.position.y = b =  -22.23; //  -1.91;
-    goal_pose_message.pose.position.z = 0;
-    goal_pose_message.pose.orientation.x = 0;
-    goal_pose_message.pose.orientation.y = 0;
-    goal_pose_message.pose.orientation.z = 0;
-    goal_pose_message.pose.orientation.w = 1;
-    
-    publisher->publish(goal_pose_message);
-    sleep(4);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-
-
-    try {
-      transformStamped = tfBuffer.lookupTransform("base_link", "map", rclcpp::Time(0));
-      } catch (tf2::TransformException &ex) {
-          RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
-          continue;
-       }
-
-   x = - transformStamped.transform.translation.x;
-   y = - transformStamped.transform.translation.y;
-
-   std::cout << "a: " << a << "     x: "    << x << std::endl;
-   std::cout << "b: " << b << "     y: "    << y << std::endl;
-
-    if (((Vx < Vo) and (Vy < Vo) and (Vx != 0)) and (((a - error) < x) and (x < (a + error))) and (((b - error) < y) and (y < (b + error)))){
-      flag3 = 0;
-      flag4 = 1;
-    }
-  }
-
-  std::cout << "Cuarto goal" << std::endl;
-
-  while (rclcpp::ok() and flag4) {
-
-    goal_pose_message.pose.position.x = a = -24.73; //  1.62; 
-    goal_pose_message.pose.position.y = b = -8.78;  //  6.08; 
-    goal_pose_message.pose.position.z = 0;
-    goal_pose_message.pose.orientation.x = 0;
-    goal_pose_message.pose.orientation.y = 0;
-    goal_pose_message.pose.orientation.z = 0;
-    goal_pose_message.pose.orientation.w = 1;
-    publisher->publish(goal_pose_message);
-    flag4 = 0;
-    sleep(4);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-
-    try {
-      transformStamped = tfBuffer.lookupTransform("base_link", "map", rclcpp::Time(0));
-      } catch (tf2::TransformException &ex) {
-          RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
-          continue;
-       }
-    x = - transformStamped.transform.translation.x;
-    y = - transformStamped.transform.translation.y;
-
-    std::cout << "a: " << a << "     x: "    << x << std::endl;
-    std::cout << "b: " << b << "     y: "    << y << std::endl;
-
-    if (((Vx < Vo) and (Vy < Vo) and (Vx != 0)) and (((a - error) < x) and (x < (a + error))) and (((b - error) < y) and (y < (b + error)))){
-      flag4 = 0;
-    }
-  }
-  
 
   rclcpp::shutdown();
   return 0;
